771362dc5e5ceca6404140d517a0b56ddc8db6e0
gigit974 <manolo97453@gmail.com>
2017-05-09 16:41:30 +0200

    initialisation

--- test/FinderTest.php
+++ test/FinderTest.php
@@ -11,4 +11,15 @@ class FinderTest extends \PHPUnit_Framework_TestCase
     public function testOk() {
         $this->assertTrue(true);
     }
+
+    public function testEssai(){
+
+    	$a = new Finder('bananna', ['apple', 'banana', 'kiwi']);
+
+		// Get first similar word (it's banana)
+		// echo $a->first();
+
+		$this->assertEquals( "bananna", $a->first() );
+    }
+
 }
